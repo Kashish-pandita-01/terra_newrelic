5905b66861bb22a5944772c101f102bc0bc34a7c
Kashish-pandita <kashish28021997@gmail.com>
2022-11-04 19:50:29 +0530

    For NR code modified via secrets

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "newrelic" {
-  account_id = 3659328
-  api_key = "NRAK-QEDHCML2YSWBX1H69ENB8DM99L4"    # usually prefixed with 'NRAK'
+  # account_id = 3659328
+  # api_key = "NRAK-QEDHCML2YSWBX1H69ENB8DM99L4"    # usually prefixed with 'NRAK'
   region = "US"                    # Valid regions are US and EU
 }
